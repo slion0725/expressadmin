fd073e7b1966b3359a1c1eda6f978adf7e53e8fd
miles <slion0725@gmail.com>
2017-04-24 12:52:21 +0800

    update

--- layout/gulpfile.js
+++ layout/gulpfile.js
@@ -1,67 +1,73 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var concatCss = require('gulp-concat-css');
-var sass = require('gulp-sass');
-var gulpCompass = require('gulp-compass');
-var notify = require('gulp-notify');
-var browserSync = require('browser-sync').create();
-var reload = browserSync.reload;
+var gulp = require('gulp')
+var concat = require('gulp-concat')
+var concatCss = require('gulp-concat-css')
+var sass = require('gulp-sass')
+// var gulpCompass = require('gulp-compass')
+var notify = require('gulp-notify')
+var browserSync = require('browser-sync').create()
+var reload = browserSync.reload
 
 gulp.task('js_plugins', function() {
     return gulp.src(
-      [
-        './bower_components/jquery/dist/jquery.min.js',
-        // './bower_components/bootstrap/dist/js/bootstrap.min.js',
-        './bower_components/lodash/dist/lodash.min.js',
-        './bower_components/uikit/dist/js/uikit.min.js',
-        './bower_components/uikit/dist/js/uikit-icons.min.js',
-        // './bower_components/vue/dist/vue.min.js',
-        './bower_components/vue/dist/vue.js',
-        './bower_components/holderjs/holder.min.js',
-        './bower_components/axios/dist/axios.min.js',
-      ]
-    )
-    .pipe(concat('plugins.js'))
-    .pipe(gulp.dest('./dist/js'))
-    .pipe(notify("JS Plugins Done"));
-});
+        [
+            './bower_components/jquery/dist/jquery.min.js',
+            // './bower_components/bootstrap/dist/js/bootstrap.min.js',
+            './bower_components/lodash/dist/lodash.min.js',
+            './bower_components/uikit/dist/js/uikit.min.js',
+            './bower_components/uikit/dist/js/uikit-icons.min.js',
+            // './bower_components/vue/dist/vue.min.js',
+            './bower_components/vue/dist/vue.js',
+            './bower_components/holderjs/holder.min.js',
+            './bower_components/axios/dist/axios.min.js',
+        ]
+      )
+      .pipe(concat('plugins.js'))
+      .pipe(gulp.dest('./dist/js'))
+      .pipe(notify('JS Plugins Done'))
+})
 
 gulp.task('css_plugins', function() {
     return gulp.src(
-      [
-        // './bower_components/bootstrap/dist/css/bootstrap.min.css',
-        './bower_components/uikit/dist/css/uikit.min.css',
-      ]
-    )
-    .pipe(concatCss("plugins.css"))
-    .pipe(gulp.dest('./dist/css'))
-    .pipe(notify("CSS Plugins Done"));
-});
+        [
+            // './bower_components/bootstrap/dist/css/bootstrap.min.css',
+            './bower_components/uikit/dist/css/uikit.min.css',
+        ]
+      )
+      .pipe(concatCss('plugins.css'))
+      .pipe(gulp.dest('./dist/css'))
+      .pipe(notify('CSS Plugins Done'))
+})
 
 gulp.task('bootstrap', function() {
-    gulp.src('./bower_components/bootstrap/dist/fonts/*.*', {base: 'bower_components/bootstrap/dist'}).pipe(gulp.dest('./dist'));
-});
+    gulp.src('./bower_components/bootstrap/dist/fonts/*.*', {
+        base: 'bower_components/bootstrap/dist'
+    }).pipe(gulp.dest('./dist'))
+})
 
 gulp.task('files_plugins', ['bootstrap'], function() {
-    gulp.src('').pipe(notify("Files Plugins Done"));
-});
+    gulp.src('').pipe(notify('Files Plugins Done'))
+})
 
 gulp.task('scss', function() {
     return gulp.src('./dist/scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist/css'))
-    // .pipe(gulpCompass({css: './dist/css', sass: './scss', image: './dist/images'}))
-    // .on('error', function(error) {
-    //   console.log(error);
-    //   this.emit('end');
-    // })
-    .pipe(reload({stream: true}))
-    .pipe(notify("Scss Done"));
-});
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./dist/css'))
+        // .pipe(gulpCompass({css: './dist/css', sass: './scss', image: './dist/images'}))
+        // .on('error', function(error) {
+        //   console.log(error);
+        //   this.emit('end');
+        // })
+        .pipe(reload({
+            stream: true
+        }))
+        .pipe(notify('Scss Done'))
+})
 
 gulp.task('scripts', function() {
-    return gulp.src('./dist/js/**/*.js').pipe(reload({stream: true})).pipe(notify("Scripts Done"));
-});
+    return gulp.src('./dist/js/**/*.js').pipe(reload({
+        stream: true
+    })).pipe(notify('Scripts Done'))
+})
 
 gulp.task('serve', ['scss'], function() {
     browserSync.init({
@@ -77,13 +83,13 @@ gulp.task('serve', ['scss'], function() {
             forms: true,
             scroll: true
         }
-    });
-});
+    })
+})
 
 gulp.task('watch', function() {
-    gulp.watch('./dist/js/*.js', ['scripts']);
-    gulp.watch('./dist/scss/**/*.scss', ['scss']);
-    gulp.watch("./*.html").on('change', reload);
-});
+    gulp.watch('./dist/js/*.js', ['scripts'])
+    gulp.watch('./dist/scss/**/*.scss', ['scss'])
+    gulp.watch('./*.html').on('change', reload)
+})
 
-gulp.task('default', ['serve', 'watch']);
+gulp.task('default', ['serve', 'watch'])
